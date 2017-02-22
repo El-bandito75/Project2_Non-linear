6df15d4cad5de8678231b982e3a925ca9c5731f7
El-bandito75 <mikerevilla75@gmail.com>
2017-02-22 14:06:39 -0400

    Update cityInfo.h

--- cityInfo.h
+++ cityInfo.h
@@ -8,24 +8,23 @@ using namespace std;
 
 class cityInfo {
 protected:
-	string  name;
-	string  stateProv;  // get rid of these
-	string  country;    // only need dataCity
-	string  log;
-	string  lat;
-	string  pop; 
+
 	string  dataCity[10][6];// change to 1000
 
 public:
 	
-	string getName();
-	void setName();
-	
-	void setDataCity();
 	string getDataCity(int i);
+	string printMsg(bool beGin);
+	string getNameCity(int i);
+	int findCity(string keyWord);
+	int findEnd();
 
 	void readFile();
 	void writeFile();
+	void listCities();
+	void deleteCity(int currCitLoc);
+	void displayCity(int currCitLoc);
+	void addNewCity(int currCitLoc);
 };
 
 #endif
